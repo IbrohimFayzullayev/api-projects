5027ba8a73eb11bc4878749db123535b2ae9c927
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-03 12:21:30 +0500

    currency converter finished

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-// fetch("https://api.exchangerate.host/")
-//   .then((val) => val.json())
-//   .then((res) => console.log(res));
-// fetch(
-//   "https://api.weatherapi.com/v1/current.json?key=44d37f22c06d4dbba27165419220204&q=Toshkent&aqi=no"
-// )
-//   .then((val) => val.json())
-//   .then((res) => console.log(res));
-// fetch(
-//   "https://api.weatherapi.com/v1/current.json?key=https://api.weatherapi.com/v1/current.json?key=44d37f22c06d4dbba27165419220204&q=Navoi&aqi=no&q=London&aqi=no"
-// )
-//   .then(function (e) {
-//     console.log(e);
-//     return e.json();
-//   })
-//   .then(function (val) {
-//     console.log(val);
-//   });
-// let a = document.querySelector(".cur__1");
-// let b = document.querySelector(".cur__2");
-// console.log(a.value);
-// console.log(b.value);
+let select1 = document.querySelector(".cur__1");
+let select2 = document.querySelector(".cur__2");
+let convert = document.querySelector(".convert");
+let usetInput = document.querySelector(".currency__1");
+let result = document.querySelector(".currency__2");
+let dateText = document.querySelector(".date");
+fetch(
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (res) {
+    for (let [key, val] of Object.entries(res)) {
+      let html = `<option value="${key}">${val}</option>`;
+      select1.insertAdjacentHTML("beforeend", html);
+      select2.insertAdjacentHTML("beforeend", html);
+    }
+  });
+
+let convertCurrency = function (cur1, cur2) {
+  fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${cur1}.json`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (res) {
+      dateText.textContent = ` Updated: ${String(res.date)
+        .split("-")
+        .join(".")}`;
+      let obj;
+      for (let [key, val] of Object.entries(res)) {
+        obj = val;
+      }
+      let num;
+      for (let [key, val] of Object.entries(obj)) {
+        if (key === cur2) {
+          num = val;
+        }
+      }
+      result.value = (Number(usetInput.value) * num).toFixed(3);
+    });
+};
+
+usetInput.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    convertCurrency(select1.value, select2.value);
+  }
+});
+convert.addEventListener("click", function (e) {
+  e.preventDefault();
+  convertCurrency(select1.value, select2.value);
+});
+
+select1.addEventListener("change", function (e) {
+  e.preventDefault();
+  convertCurrency(select1.value, select2.value);
+});
+select2.addEventListener("change", function (e) {
+  e.preventDefault();
+  convertCurrency(select1.value, select2.value);
+});
+convertCurrency(select1.value, select2.value);
